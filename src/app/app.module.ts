52a0be778cb73c3aa9e5c3673efec0c805b66690
Symosko <dks0515936@depc02wq0lrhtdd.home>
2019-07-10 01:10:00 -0400

    generate all components needed for project so far, create a recipe model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './shared/header-component/header-component.component';
+import { ShoppingListComponent } from './feature/shoppingList/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './feature/shoppingList/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './feature/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './feature/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './feature/recipes/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './feature/recipes/recipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
